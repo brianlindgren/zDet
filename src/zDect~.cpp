137afdc70268be03e0382baf9d252fbc127cdf60
brianlindgren <92063518+brianlindgren@users.noreply.github.com>
2024-05-22 15:13:52 -0400

    done

--- src/zDect~.cpp
+++ src/zDect~.cpp
@@ -9,6 +9,11 @@ public:
     ~evString();
 
     float process(float in); // main loop
+    void setLowRange(float lowRange) { stringHz_ = lowRange; recalculateSettings(); }
+    void setHighRange(float highRange) { stringHzHi_ = highRange; recalculateSettings(); }
+    void setSampleRate(int sampleRate) { sampleRate_ = sampleRate; firstSetup(sampleRate_); }
+    bool isFreqOutRange() const { return freqOutRange_; }
+    bool isFreqOutDect() const { return freqOutDect_; }
 
 protected:
 private:
@@ -18,9 +23,12 @@ private:
     float stringHz_;
     float stringHzHi_;
     bool prevPitchDetectionFlag_;
+    bool freqOutRange_;
+    bool freqOutDect_;
     float sampLen_;
     float frequency_;
-    float prevFrequency_; // only needed for printing purposes now
+    float frequencyNew_;
+    float frequencyPrev_ = 0;
     float pitchDecLo_;
     float pitchDecHi_;
     int pitchDetectionTimer_;
@@ -30,14 +38,14 @@ private:
 };
 
 // Implementation of evString methods
-evString::evString(int sampleRate, float stringHz, float stringHzHi) : sampleRate_(sampleRate), stringHz_(stringHz), stringHzHi_(stringHzHi),
-                                                     prevPitchDetectionFlag_(true), sampLen_(1.0 / sampleRate),
-                                                     frequency_(0.0), prevFrequency_(0.0), pitchDetectionTimer_(0) {
+evString::evString(int sampleRate, float stringHz, float stringHzHi)
+    : sampleRate_(sampleRate), stringHz_(stringHz), stringHzHi_(stringHzHi),
+      prevPitchDetectionFlag_(true), freqOutRange_(false), freqOutDect_(false),
+      sampLen_(1.0 / sampleRate), frequency_(0.0), pitchDetectionTimer_(0) {
     firstSetup(sampleRate);
 }
 
-evString::~evString() {
-}
+evString::~evString() {}
 
 void evString::firstSetup(int rate) {
     sampleRate_ = rate;
@@ -47,38 +55,42 @@ void evString::firstSetup(int rate) {
 
 void evString::recalculateSettings() {
     // pitch detection limits
-    pitchDecLo_ = (stringHz_); // 5 Hz below
-    pitchDecHi_ = 1 / ((stringHzHi_) / sampleRate_);
+    pitchDecLo_ = stringHz_;
+    pitchDecHi_ = 1 / (stringHzHi_ / sampleRate_);
 }
 
 float evString::process(float in) {
     bool pitchDetectionFlag = false; // declare outside of if statements
 
-    // 1 is (+) wavelength, 0 is (-)
+    // true is (+) sample, false is (-)
     if (in > 0) { // 'in' is the current sample
-        pitchDetectionFlag = true; // 1 = positive sample value
-    } else if (in < 0) { // 0 = positive sample value
+        pitchDetectionFlag = true; // true = positive sample value
+    } else if (in < 0) { // false = positive sample value
         pitchDetectionFlag = false;
     }
 
     // if we have a zero crossing and the wave is coming up from below the zero line
-    if (pitchDetectionFlag != prevPitchDetectionFlag_ && prevPitchDetectionFlag_ == false
+    if (pitchDetectionFlag != prevPitchDetectionFlag_ && !prevPitchDetectionFlag_
         && pitchDetectionTimer_ >= pitchDecHi_) {
+
         float freq = 1 / (pitchDetectionTimer_ * sampLen_);
 
         // if the calculated frequency is above the string's tuned pitch, let's use it!
         if (freq > pitchDecLo_) {
-            frequency_ = freq;
-
-            // only for printing purposes. print if new freq is more than 2 Hz different than old
-            if (fabsf(prevFrequency_ - frequency_) > 2) {
-                // Remove the printf call since it might not be suitable for Pure Data
-                // printf("Pitch is %f:\n", frequency_);
-            }
-
-            prevFrequency_ = frequency_;
+           	frequencyPrev_ = frequency_;
+           	frequencyNew_ = freq;
+            frequency_ = (frequencyPrev_ + frequencyNew_) / 2;
+            
+            freqOutRange_ = true;
+            freqOutDect_ = true;
+        } else {
+            freqOutRange_ = false;
+            freqOutDect_ = false;
         }
+
         pitchDetectionTimer_ = 0; // reset timer regardless if the frequency was too low
+    } else {
+        freqOutDect_ = false;
     }
 
     // needs to be outside of an if/else. need to run every sample
@@ -96,21 +108,33 @@ typedef struct _zDect {
     t_sample f;
     evString *string;
     t_float detection_on; // 0 for off, 1 for on
+    t_float low_range;
+    t_float high_range;
+    t_outlet *msg_outlet; // outlet for frequency message
+    t_outlet *range_outlet; // outlet for frequency in range
+    t_outlet *dect_outlet; // outlet for detection status
 } t_zDect;
 
 // Constructor
 void *zDect_new(t_floatarg hz, t_floatarg hzHi) {
     t_zDect *x = (t_zDect *)pd_new(zDect_class);
 
-    // Create a control inlet for detection on/off
+    // Create inlets for low and high range, and detection on/off
+    floatinlet_new(&x->x_obj, &x->low_range);
+    floatinlet_new(&x->x_obj, &x->high_range);
     floatinlet_new(&x->x_obj, &x->detection_on);
 
-    // Create a signal outlet
+    // Create signal and message outlets
     outlet_new(&x->x_obj, &s_signal);
+    x->msg_outlet = outlet_new(&x->x_obj, &s_float);
+    x->range_outlet = outlet_new(&x->x_obj, &s_float);
+    x->dect_outlet = outlet_new(&x->x_obj, &s_float);
 
-    // Initialize evString with the provided frequencies or default to 130 Hz and 880 Hz
-    x->string = new evString(sys_getsr(), hz != 0 ? hz : 130, hzHi != 0 ? hzHi : 880);
+    // Initialize evString with the provided frequencies or default to 20 Hz and 4000 Hz
+    x->string = new evString(sys_getsr(), hz != 0 ? hz : 20, hzHi != 0 ? hzHi : 4000);
     x->detection_on = 1; // start with detection on
+    x->low_range = hz != 0 ? hz : 20;
+    x->high_range = hzHi != 0 ? hzHi : 4000;
 
     return (void *)x;
 }
@@ -127,12 +151,30 @@ t_int *zDect_perform(t_int *w) {
     t_sample *out = (t_sample *)(w[3]);
     int n = (int)(w[4]);
 
+    float frequency = 0.0;
+    bool freqOutRange = false;
+    bool freqOutDect = false;
+
+    // Update low and high range settings
+    x->string->setLowRange(x->low_range);
+    x->string->setHighRange(x->high_range);
+
     for (int i = 0; i < n; i++) {
         if (x->detection_on) {
-            out[i] = x->string->process(in[i]);
+            frequency = x->string->process(in[i]);
+            freqOutRange = x->string->isFreqOutRange();
+            freqOutDect = x->string->isFreqOutDect();
+            out[i] = frequency;
         } else {
             out[i] = 0; // output 0 when detection is off
         }
+    }
+
+    if (x->detection_on) {
+        // Send frequency as a message
+        outlet_float(x->msg_outlet, frequency);
+        outlet_float(x->range_outlet, freqOutRange);
+        outlet_float(x->dect_outlet, freqOutDect);
     }
 
     return (w + 5);
@@ -140,6 +182,7 @@ t_int *zDect_perform(t_int *w) {
 
 // DSP Setup
 void zDect_dsp(t_zDect *x, t_signal **sp) {
+    x->string->setSampleRate(sp[0]->s_sr); // Update sample rate
     dsp_add(zDect_perform, 4, x, sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
 }
 
